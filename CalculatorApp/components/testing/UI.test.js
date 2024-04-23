99b2076a659698dc92ccffef6b1d1dc411ce1b2e
Antonio Nitoi <64855012+4N70N1U5@users.noreply.github.com>
2024-04-23 17:17:28 +0300

    Add new UI tests to check all components render on home screen

--- CalculatorApp/components/testing/UI.test.js
+++ CalculatorApp/components/testing/UI.test.js
@@ -6,18 +6,89 @@ import Button from "../Button";
 import TextBox from "../TextBox";
 
 describe("UI Testing Suite", () => {
-  test("Text box renders correctly", () => {
-    const tree = renderer.create(<TextBox />).toJSON();
+  test("Text box component matches snapshot", () => {
+    const tree = renderer.create(<TextBox> 1234 </TextBox>).toJSON();
     expect(tree).toMatchSnapshot();
   });
 
-  test("Buttons render correctly", () => {
+  test("Button component matches snapshot", () => {
     const tree = renderer.create(<Button />).toJSON();
     expect(tree).toMatchSnapshot();
   });
 
-  test("Home screen renders correctly", () => {
+  test("Home screen matches snapshot", () => {
     const tree = renderer.create(<Home />).toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  test("Home screen renders everything", () => {
+    const { getByText, getByTestId } = render(<Home />);
+
+    const textBox = getByTestId("text-input");
+    expect(textBox).toBeDefined();
+
+    const clearButton = getByText("C");
+    expect(clearButton).toBeDefined();
+
+    const ACButton = getByText("AC");
+    expect(ACButton).toBeDefined();
+
+    const percentageButton = getByText("%");
+    expect(percentageButton).toBeDefined();
+
+    const divideButton = getByText("/");
+    expect(divideButton).toBeDefined();
+
+    const multiplyButton = getByText("x");
+    expect(multiplyButton).toBeDefined();
+
+    const subtractButton = getByText("-");
+    expect(subtractButton).toBeDefined();
+
+    const addButton = getByText("+");
+    expect(addButton).toBeDefined();
+
+    const equalsButton = getByText("=");
+    expect(equalsButton).toBeDefined();
+
+    const decimalButton = getByText(".");
+    expect(decimalButton).toBeDefined();
+
+    const zeroButton = getByText("0");
+    expect(zeroButton).toBeDefined();
+
+    const oneButton = getByText("1");
+    expect(oneButton).toBeDefined();
+
+    const twoButton = getByText("2");
+    expect(twoButton).toBeDefined();
+
+    const threeButton = getByText("3");
+    expect(threeButton).toBeDefined();
+
+    const fourButton = getByText("4");
+    expect(fourButton).toBeDefined();
+
+    const fiveButton = getByText("5");
+    expect(fiveButton).toBeDefined();
+
+    const sixButton = getByText("6");
+    expect(sixButton).toBeDefined();
+
+    const sevenButton = getByText("7");
+    expect(sevenButton).toBeDefined();
+
+    const eightButton = getByText("8");
+    expect(eightButton).toBeDefined();
+
+    const nineButton = getByText("9");
+    expect(nineButton).toBeDefined();
+
+    fireEvent.press(oneButton);
+    fireEvent.press(twoButton);
+    fireEvent.press(threeButton);
+    fireEvent.press(fourButton);
+
+    expect(textBox.props.value).toEqual("1234");
   });
 });
